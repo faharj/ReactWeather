a0aeda84f6021c4c41f3cd7e4d52c96a1e58a195
Unsa Erum Sahato <Unsa@Unsas-MacBook-Air.local>
2017-02-01 11:58:24 -0800

    Prep for heroku 2

--- server.js
+++ server.js
@@ -4,20 +4,16 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next){
   if (req.headers['x-forwarded-proto'] === 'http') {
     next();
   } else {
     res.redirect('http://' + req.hostname + req.url);
   }
-})
-
-app.use(function (req, res) {
-
-})
+});
 
 app.use(express.static('public'));
 
 app.listen(PORT, function () {
-  console.log('Express server is up on port 3000 ' + PORT);
+  console.log('Express server is up on port ' + PORT);
 });
