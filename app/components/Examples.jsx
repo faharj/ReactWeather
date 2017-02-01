2f03dfb46420deae39146f636d416b6274c2be96
Unsa Erum Sahato <Unsa@Unsas-MacBook-Air.local>
2017-02-01 11:26:55 -0800

    Initial Commit

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,17 +1,12 @@
 var React = require('react');
 
-//var Examples = React.createClass({
-//    render: function(){
-//        return (
-//            <h3>Examples component</h3>
-//        );
-//    }
-//})
-
 var Examples = (props) => {
-    return (
-            <h3>Examples component</h3>
-        );
-}
+  return (
+    <div>
+      <h3>Examples Component!</h3>
+      <p>Welcome to examples page!</p>
+    </div>
+  )
+};
 
 module.exports = Examples;
